45700ab09e7a63be6c39e8cabe55f5fe3394fee4
Leleu Vilela <viniciusvilela19@gmail.com>
2024-07-25 17:03:54 +0100

    add CopilotChat keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,9 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-local Util = require("lazyvim.util")
-
-local map = Util.safe_keymap_set
+local map = vim.keymap.set
 
 map("n", "\\", ":Neotree toggle reveal_force_cwd<CR>", { desc = "Neotree toggle" })
 map("n", "|", ":Neotree reveal<CR>", { desc = "Neotree reveal" })
@@ -12,3 +10,14 @@ map("n", "<leader>bf", ":Neotree reveal buffers float<CR>", { desc = "Neotree bu
 
 
 map("n", "<leader>h", ":nohlsearch<CR>", { desc = "Clear highlights" })
+
+local openCopilot = function()
+    local input = vim.fn.input("Quick Chat: ")
+    if input ~= "" then
+        require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
+    end
+end
+
+map({"n", "v"}, "<leader>cq", openCopilot, { desc = "CopilotChat - Quick chat", remap = true })
+map({"n", "v"}, "<leader>cQ", ":CopilotChat", { desc = "CopilotChat - Open chat", remap = true })
+
